a4b8730565774efe5f48b3c5b514323af182cb9d
Jeather Palma <jeather_alfonso.palma@janusconsultores.com.mx>
2023-01-12 16:12:30 -0600

    Test pruiebas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,42 @@ export class AppComponent implements OnInit {
   title = 'geolocation';
   public latitude;
   public longitude;
+  //for tracking
+  HIGH_ACCURACY: boolean = true;
+  MAX_CACHE_AGE_MILLISECOND: number = 1;
+  MAX_NEW_POSITION_MILLISECOND: number = 5000;
+
+  trackOptions = {
+    enableHighAccuracy: this.HIGH_ACCURACY,
+    maximumAge: this.MAX_CACHE_AGE_MILLISECOND,
+    timeout: this.MAX_NEW_POSITION_MILLISECOND,
+  };
   constructor(public locationService: LocationService) {
 
   }
   ngOnInit() {
-    let location = this.getLocation();
+    //let location = this.getLocation();
+    this.startTracking();
   }
   getLocation() {
-    this.locationService.startTracking().then(pos => {
+    this.locationService.getPosition().then(pos => {
       this.latitude = pos.lat;
       this.longitude = pos.lng;
     });
+  }
+
+  startTracking(){
+  
+    navigator.geolocation.watchPosition((resp) => {
+      
+      console.log("Leyo leyo");
+      
+      this.latitude = resp.coords.latitude; 
+      this.longitude =  resp.coords.longitude
+      
+    }, (err) => {
+      console.log(err);
+      
+    }, this.trackOptions)
   }
 }
--- src/app/location.service.ts
+++ src/app/location.service.ts
@@ -6,16 +6,7 @@ import { rejects } from 'assert';
 })
 export class LocationService {
 
-  //for tracking
-  HIGH_ACCURACY: boolean = true;
-  MAX_CACHE_AGE_MILLISECOND: number = 30000;
-  MAX_NEW_POSITION_MILLISECOND: number = 5000;
-
-  trackOptions = {
-    enableHighAccuracy: this.HIGH_ACCURACY,
-    maximumAge: this.MAX_CACHE_AGE_MILLISECOND,
-    timeout: this.MAX_NEW_POSITION_MILLISECOND,
-  };
+  
 
   constructor() { }
 
@@ -30,15 +21,7 @@ export class LocationService {
     });
   }
 
-  startTracking(): Promise<any>{
-    return new Promise((resolve, reject) => {
-      navigator.geolocation.watchPosition((resp) => {
-        resolve({ lng: resp.coords.longitude, lat: resp.coords.latitude });
-        
-      }, (err) => {
-        reject(err);
-      }, this.trackOptions)
-    });
   
-  }
+  
+  
 }
